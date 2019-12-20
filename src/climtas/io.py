7fbc06d71090142e40a8d989a48d0f5e35d7687c
Scott Wales <scott.wales@unimelb.edu.au>
2019-12-20 14:54:47 +1100

    Add some comments

--- src/climtas/io.py
+++ src/climtas/io.py
@@ -44,6 +44,7 @@ def to_netcdf_throttled(ds, path, complevel=4, max_tasks=None, show_progress=Tru
     if isinstance(ds, xarray.DataArray):
         ds = ds.to_dataset()
 
+    # Setup compression and chunking
     for k, v in ds.data_vars.items():
         encoding[k] = {
             "zlib": True,
@@ -52,18 +53,33 @@ def to_netcdf_throttled(ds, path, complevel=4, max_tasks=None, show_progress=Tru
             "chunksizes": getattr(v.data, "chunksize", None),
         }
 
+    # Prepare storing the data to netcdf, but don't evaluate
     f = ds.to_netcdf(str(path), encoding=encoding, compute=False)
+
+    # This is some very low-level dask operations. behind the scenes dask
+    # stores its objects as a graph of operations and their dependencies.
+    # We're going to grab a specific operation, 'dask.array.core.store_chunk',
+    # and run each instance of that operation in a throttled manner, so they
+    # don't all just get submitted at once and overwhelm memory, at the expense
+    # of having to do stuff like reading input multiple times rather than just
+    # once.
+
+    # We also need to make a new graph, where the tasks that have 'store_chunk'
+    # as a dependency know that their pre-requisite has been completed. To do
+    # this we just need to fix up the 'store_chunk' tasks, other tasks that
+    # 'store_chunk' depends on will be automatically cleaned up when dask
+    # optimises the graph
 
     old_graph = f.__dask_graph__()
     new_graph = {}
     store_keys = []
 
-    # Pull out the 'store_chunk' operations from the graph and run them
-    # throttled
+    # Pull out the 'store_chunk' operations from the graph and put them in a
+    # list
     for k, v in old_graph.items():
         if v[0] == dask.array.core.store_chunk:
-            new_graph[k] = None
             store_keys.append(k)
+            new_graph[k] = None  # Mark the task done in new_graph
             continue
         new_graph[k] = v
 
@@ -72,7 +88,8 @@ def to_netcdf_throttled(ds, path, complevel=4, max_tasks=None, show_progress=Tru
 
         store_keys = tqdm(store_keys)
 
+    # Run the 'store_chunk' tasks with 'old_graph'
     throttle_futures(old_graph, store_keys, max_tasks=None)
 
-    # Finalise
-    ff = optimized_dask_get(new_graph, list(f.__dask_layers__()))
+    # Finalise any remaining operations with 'new_graph'
+    return optimized_dask_get(new_graph, list(f.__dask_layers__()))
