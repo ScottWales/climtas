9ae72782a5578bea5e44466322832450cb0447cb
Scott Wales <scott.wales@unimelb.edu.au>
2019-12-20 17:34:54 +1100

    Set worker dir

--- test/conftest.py
+++ test/conftest.py
@@ -2,7 +2,7 @@ import pytest
 import dask
 
 @pytest.fixture
-def distributed_client():
-    c = dask.distributed.Client(n_workers=1, threads_per_worker=1)
+def distributed_client(tmpdir):
+    c = dask.distributed.Client(n_workers=1, threads_per_worker=1, local_directory=tmpdir/'dask-worker-space')
     yield c
     c.close()
