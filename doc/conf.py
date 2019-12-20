9eef47fb845c9ff8e52f3080543ae617dd37c9de
Scott Wales <scott.wales@unimelb.edu.au>
2019-12-20 18:02:33 +1100

    Enable napoleon

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ release = climtas.__version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest", "sphinx.ext.coverage"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest", "sphinx.ext.coverage", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
